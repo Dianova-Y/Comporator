e4e15bce54cba922535c6c89649509230dac525e
Yulia Dianova <dianova@internet.ru>
2022-09-17 17:23:18 +0300

    Add lambda branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -12,7 +11,19 @@ public class Main {
         people.add(new Person("Федор", "Ван Дер Варт", 32));
         people.add(new Person("Василий", "Триндаде де Вильена", 42));
 
-        Collections.sort(people, new PersonSurnameComporator(5));
+        // Collections.sort(people, new PersonSurnameComporator(5));
+
+        people.sort((o1, o2) -> {
+                    int word1 = o1.getSurname().split(" ").length;
+                    int word2 = o2.getSurname().split(" ").length;
+
+                    if (word1 == word2 || word1 > 5 && word2 > 5) {
+                        return Integer.compare(o1.getAge(), o2.getAge());
+                    }
+                    return Integer.compare(word1, word2);
+                }
+        );
+        
         System.out.println(people);
     }
 }
